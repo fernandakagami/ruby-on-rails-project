ac8b502b3b8c4a5d4a3d128b0baf2528465e3849
Fernanda <fernandakagami@gmail.com>
2022-10-20 19:16:22 -0300

    update ruby essentials

---
+++ ruby_essencial/01_IO.rb
@@ -0,0 +1,14 @@
+puts "Olá Curso de Rails"
+
+puts "Digite seu nome:"
+nome = gets
+puts "O seu nome é " + nome
+
+puts nome.inspect
+
+puts "===================="
+
+puts "Digite seu sobrenome:"
+sobrenome = gets.chomp
+
+puts sobrenome.inspect
