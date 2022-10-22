664fe7ca5dbff32d760663be6e3cc9415aa1f9fb
Fernanda <fernandakagami@gmail.com>
2022-10-21 21:06:12 -0300

    Update 01_IO.rb

--- ruby_essencial/01_IO.rb
+++ ruby_essencial/01_IO.rb
@@ -6,9 +6,18 @@ puts "O seu nome é " + nome
 
 puts nome.inspect
 
+
 puts "===================="
 
 puts "Digite seu sobrenome:"
 sobrenome = gets.chomp
 
 puts sobrenome.inspect
+
+
+puts "===================="
+
+puts "Digite seu salário:"
+salario = gets.chomp.to_f
+
+puts "Seu salário atualizado é " + (salario * 1.10).to_s
